21c668e57b88484dc6f3a39273840b3e669132cd
Tim Bacon <tim.bacon@vetprod.com>
2023-06-29 13:46:52 +0200

    flash message done

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,20 +1,34 @@
 'use client';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useContext } from 'react';
+import { ToastContext } from '../../../../components/ToastProvider/ToastProvider';
 
 function ContactPage() {
-  return (
-    <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+    const router = useRouter();
+    const { createToast } = useContext(ToastContext);
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+    const handleSubmit = event => {
+        event.preventDefault();
 
-        <button>Submit</button>
-      </form>
-    </main>
-  );
+        router.push('/exercises/02-flash-messages');
+        createToast(
+            "Your message was received. We'll get back to you shortly",
+            'success'
+        );
+    };
+    return (
+        <main>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='name'>Name:</label>
+                <input id='name' required={true} />
+
+                <label htmlFor='message'>Message:</label>
+                <textarea id='message' />
+
+                <button>Submit</button>
+            </form>
+        </main>
+    );
 }
 
 export default ContactPage;
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import ToastProvider from '../../../components/ToastProvider/ToastProvider';
+import ToastShelf from '../../../components/ToastShelf/ToastShelf';
 import './styles.css';
 
 function FlashMsgLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body>
+                <ToastProvider>
+                    {children}
+                    <ToastShelf />
+                </ToastProvider>
+            </body>
+        </html>
+    );
 }
 
 export default FlashMsgLayout;
