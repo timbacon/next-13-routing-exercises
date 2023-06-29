28fa603707c071a2508f222df7343bbb7d61d568
Tim Bacon <tim.bacon@vetprod.com>
2023-06-29 13:12:10 +0200

    screensaver exercise finished

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
-import ScreenSaver from '../../../components/ScreenSaver';
-
 function ScreenSaverExercise() {
-  return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
-    </main>
-  );
+    return (
+        <main className='screen-saver-color-wrapper'>
+            <h1>Choose your color:</h1>
+            <ul>
+                {['red', 'blue', 'cyan', 'hotpink', 'magenta'].map(color => {
+                    return (
+                        <li key={color}>
+                            <Link href={`/exercises/01-screensaver/${color}`}>
+                                {color}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </main>
+    );
 }
 
 export default ScreenSaverExercise;
